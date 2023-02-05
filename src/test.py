e02f235efe67c8a39f5e7ac98d31e2a4f84fb181
SimonProDev <73559027+SimonProDev@users.noreply.github.com>
2023-02-05 20:42:32 +0100

    First test backtesting

--- src/test.py
+++ src/test.py
@@ -1,2 +1,13 @@
-print('test git')
-print('test git2')
+import pandas as pd
+from IPython.display import display
+import yfinance as yf
+
+
+df = yf.download('MSCI',
+                 start='2022-01-01',
+                 end='2023-01-01',
+                 actions=True).drop(columns=['Open', 'High', 'Low', 'Volume', 'Stock Splits'])
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+display(df)
