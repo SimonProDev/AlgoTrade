429028f0e380fc54c7ee075cdb85b7ebc7580b39
SimonProDev <73559027+SimonProDev@users.noreply.github.com>
2023-04-10 19:41:55 +0200

    test 2023-04-10

---
+++ dev/dev_1.py
@@ -0,0 +1,12 @@
+string = 'Hello World!'
+
+print(f'{string[:]=}')
+print(f'{string[2:4]=}')
+print(f'{string[:-1]=}')
+print(f'{string[-2:]=}')
+print()
+print(f'{string[0:len(string):1]=}')
+print(f'{string[::]=}')
+print(f'{string[-1::-1]=}')
+print(f'{string[::2]=}')
+
