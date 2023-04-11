f9bf8352ddef7dd2d5af9c3c6ee1c548e439ed23
SimonProDev <73559027+SimonProDev@users.noreply.github.com>
2023-04-11 09:45:20 +0200

    start developping yf_manager class

---
+++ src/yf_api/fetch_data.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+
+
+class YahooFinanceManager:
+    """
+    interface with yahoo finance API
+    To be developed when functions are developed
+    :param:
+    """
+
+    def __init__(self):
+        self.name = 'yf_manager'
+
+
