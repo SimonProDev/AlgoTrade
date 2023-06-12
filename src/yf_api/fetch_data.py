69aab01c464db0a747d610ac00e6d74fdfba14b8
SimonProDev <73559027+SimonProDev@users.noreply.github.com>
2023-06-12 18:02:48 +0200

    test on selecting data on df. Adjustment for end date

--- src/yf_api/fetch_data.py
+++ src/yf_api/fetch_data.py
@@ -12,17 +12,17 @@ class YahooFinanceAPI:
     def __init__(self):
         self.ticker_data = None
         self.ticker_settings = {
-            'ticker_name': '',
-            'start_date': '',
-            'end_date': '',
-            'interval': ''
+            'ticker_name': None,
+            'start_date': None,
+            'end_date': None,
+            'interval': None
         }
 
     def set_ticker_settings(self,
-                            ticker_name: str = '',
-                            start_date: str = '',
-                            end_date: str = '',
-                            interval: str = '') -> None:
+                            ticker_name: str = None,
+                            start_date: str = None,
+                            end_date: str = None,
+                            interval: str = None) -> None:
         new_settings = locals()
         new_settings = {k: new_settings[k] for k in new_settings if k in ('ticker_name',
                                                                           'start_date',
@@ -34,20 +34,28 @@ class YahooFinanceAPI:
         """
         Download data from yahoo finance API
         """
-
-        self.ticker_data = yf.download(tickers=self.ticker_settings['ticker_name'],
-                                       start=self.ticker_settings['start_date'],
-                                       end=self.ticker_settings['end_date'],
-                                       interval=self.ticker_settings['interval'])
+        if self.ticker_settings['end_date']:
+            self.ticker_data = yf.download(tickers=self.ticker_settings['ticker_name'],
+                                           start=self.ticker_settings['start_date'],
+                                           end=self.ticker_settings['end_date'],
+                                           interval=self.ticker_settings['interval'])
+        else:
+            self.ticker_data = yf.download(tickers=self.ticker_settings['ticker_name'],
+                                           start=self.ticker_settings['start_date'],
+                                           interval=self.ticker_settings['interval'])
 
     def get_ticker_data(self) -> pd.DataFrame:
         return self.ticker_data
 
 
+pd.set_option('display.max_columns', None)
+
 yf_manager = YahooFinanceAPI()
-yf_manager.set_ticker_settings('^GDAXI', '2023-06-01', '2023-06-06', '1h')
+# yf_manager.set_ticker_settings('^GDAXI', '2023-06-05', '2023-06-12', '1h')
+yf_manager.set_ticker_settings('^GDAXI', '2023-06-05', None, '1h')
 yf_manager.download_ticker_data()
 df_res = yf_manager.get_ticker_data()
 
+
 print(df_res)
-print(yf_manager.ticker_settings)
+# print(df_res.loc['2023-06-05 10:00:00+02:00':'2023-06-05 17:00:00+02:00'])
