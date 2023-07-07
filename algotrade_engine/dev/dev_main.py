62ea11d71c53efa0792521e6c2a664c2de996c85
sbarghi <sbarghi@oney.fr>
2023-07-07 18:32:32 +0200

    dev of algotrade manager

--- algotrade_engine/dev/dev_main.py
+++ algotrade_engine/dev/dev_main.py
@@ -1,28 +1,33 @@
 import algotrade_engine.conf.settings as settings
 import importlib
 
-from algotrade_engine.src.yf_api.yf_manager import YahooFinanceManager
+from algotrade_engine.src.algotrade_manager import AlgoTradeManager
+
 from algotrade_engine.src.tools.display_df import display
 import pandas as pd
-from algotrade_engine.src.strategies.swing import Swing
+
 
 # run config
 importlib.import_module('algotrade_engine.conf.config')
 
-yf_manager = YahooFinanceManager()
-yf_manager.call_yf_api()
-output_yf_api = yf_manager.get_ticker_data()
+# yf_manager = YahooFinanceManager()
+# yf_manager.call_yf_api()
+# output_yf_api = yf_manager.get_ticker_data()
 
 
 pd.set_option('display.max_rows', None)
 
-my_ticker = output_yf_api.get('^GDAXI')
+# my_ticker = output_yf_api.get('^GDAXI')
+#
+# my_strategy = Swing(my_ticker.get_df().copy())
+# my_strategy.build_strategy()
+# # my_ticker.df = my_ticker.df.assign(new_col=lambda x: 'df_ticker')
+# # my_ticker.strategy.df = my_ticker.strategy.df.assign(new_col=lambda x: 'df_strategy')
+# # display(my_ticker.get_df(), name='my_ticker.df')
+# # display(my_strategy.df, name='my_strategy.df')
+# display(my_strategy.df)
 
-my_strategy = Swing(my_ticker.get_df().copy())
-my_strategy.build_strategy()
-# my_ticker.df = my_ticker.df.assign(new_col=lambda x: 'df_ticker')
-# my_ticker.strategy.df = my_ticker.strategy.df.assign(new_col=lambda x: 'df_strategy')
-# display(my_ticker.get_df(), name='my_ticker.df')
-# display(my_strategy.df, name='my_strategy.df')
-display(my_strategy.df)
+algotrade_manager = AlgoTradeManager()
+algotrade_manager.run_algotrade_app()
+display(algotrade_manager.strategy.df)
 print()
--- algotrade_engine/src/algotrade_manager.py
+++ algotrade_engine/src/algotrade_manager.py
@@ -1,3 +1,5 @@
+from algotrade_engine.src.yf_api.yf_manager import YahooFinanceManager
+from algotrade_engine.src.strategies.swing import Swing
 from algotrade_engine.src.alerting.alerting_manager import AlertingManager
 
 
@@ -7,12 +9,31 @@ class AlgoTradeManager:
     """
 
     def __init__(self):
-        self.alerting_manager = AlertingManager()
+        self.ticker_data = None
+        self.yf_manager = None
+        self.strategy = None
+        self.alerting_manager = None
 
     def run_algotrade_app(self):
-        self.run_alerting()
+        self.download_ticker_data()
+        self.run_strategy()
+        # self.run_alerting()
+
+    def download_ticker_data(self):
+        self.yf_manager = YahooFinanceManager()
+        self.yf_manager.call_yf_api()
+        self.ticker_data = self.yf_manager.get_ticker_data()
+
+    """
+    rework of strategy class
+    should be done on several Tickers instance
+    adjust constructor input to Strategy
+    """
+    def run_strategy(self):
+        self.strategy = Swing(self.ticker_data.get('^GDAXI').get_df().copy())
+        self.strategy.build_strategy()
 
     def run_alerting(self):
-        alterting_manager = AlertingManager()
-        alterting_manager.create_alters()
-        alterting_manager.send_alerts()
+        self.alerting_manager = AlertingManager()
+        self.alerting_manager.create_alters()
+        self.alerting_manager.send_alerts()
