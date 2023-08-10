22696ca524a90e2a6be8dfb52f466a2227d0e1cd
SimonProDev <73559027+SimonProDev@users.noreply.github.com>
2023-08-10 22:34:08 +0200

    stop tracking dev_1

--- algotrade_engine/dev/dev_1.py
+++ algotrade_engine/dev/dev_1.py
@@ -1,22 +1,8 @@
-# from datetime import datetime, time
-# import pytz
-#
-#
-# tz_paris = pytz.timezone('Europe/Paris')
-#
-# start = '16:19:00'
-# end = '16:20:00'
-# now = datetime.now(tz_paris).strftime("%H:%M:%S")
-#
-# print(f'start = {start} and now = {now}')
-# print()
-# print(now > start)
-# print(now < end)
+import pandas as pd
 
-import yfinance as yf
 
-df = yf.download(tickers=['^DJI'],
-                 start='2023-08-06',
-                 end=None,
-                 interval='30m')
-print(df)
+df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
+                   'mask': ['red', 'purple'],
+                   'weapon': ['sai', 'bo staff']})
+
+df.to_csv('../tmp_files/df.csv')
